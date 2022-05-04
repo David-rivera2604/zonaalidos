29c8298944e2645a9b046b6fab28854370d533d9
nesoler <nsoler@grupoinmotion.com>
2022-05-04 09:30:49 -0600

    pagos

---
+++ Core/Architect.Payment.Integrations/Business/OnlinePayment.cs
@@ -0,0 +1,49 @@
+﻿using Architect.Utilities.Extensions;
+using System;
+using System.Collections.Generic;
+
+namespace Architect.Payment.Integrations.Business
+{
+    /// <summary>
+    /// Pagos en línea
+    /// </summary>
+    public static partial class OnlinePayment
+    {
+
+        /// <summary>
+        /// Crea un registro en la tabla OnlinePayment.
+        /// </summary>
+        public static Contracts.OnlinePayment Create(int companyId, int userId, Contracts.OnlinePayment item)
+        {
+            Contracts.OnlinePayment result = item;
+    
+                if (result.Id.IsEmpty())
+                {
+                    result.Id = DataAccess.OnlinePayment.RetrieveLastKey() + 1;
+                }
+                result.CompanyId = companyId;
+                result.UpdateUserCode = userId;
+                result.UpdateDate = DateTime.Now;
+
+            DataAccess.OnlinePayment.Create(result);
+       
+            return result;
+        }
+
+        /// <summary>
+        /// Recupera un registro en la tabla OnlinePayment por medio del campo RequestID.
+        /// </summary>
+        public static Contracts.OnlinePayment RetrieveByRequestID(int companyId, Int64 requestId)
+        {
+            return DataAccess.OnlinePayment.RetrieveByRequestID(requestId, companyId );
+        }
+
+        /// <summary>
+        /// Actualiza un registro en la tabla OnlinePayment por medio de su clave primaria.
+        /// </summary>
+        public static int Update(Contracts.OnlinePayment onlinepaymentItem)
+        {
+            return DataAccess.OnlinePayment.Update(onlinepaymentItem);
+        }
+    }
+}
---
+++ Core/Architect.Payment.Integrations/Contracts/OnlinePayment.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Architect.Payment.Integrations.Contracts
+{
+    /// <summary>
+    /// Pagos en línea
+    /// </summary>
+    public partial class OnlinePayment
+    {
+        /// <summary>
+        /// Identificación única del pago.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Identificación de la compañía propietaria.
+        /// </summary>
+        public int CompanyId { get; set; }
+        /// <summary>
+        /// Tipo de documento.
+        /// </summary>
+        public int DocumentType { get; set; }
+        /// <summary>
+        /// Descripción para tipo de documento.
+        /// </summary>
+        public string DocumentTypeDesc { get; set; }
+        /// <summary>
+        /// Documento o número de identificación.
+        /// </summary>
+        public string DocumentNumber { get; set; }
+        /// <summary>
+        /// Primer nombre.
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Primer apellido.
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Dirección de correo electrónico (principal)
+        /// </summary>
+        public string PrimaryEmailAddress { get; set; }
+        /// <summary>
+        /// Número de teléfono del celular.
+        /// </summary>
+        public string PhoneNumberMobile { get; set; }
+        public string PolicyId { get; set; }
+        public Int64 BillNumber { get; set; }
+        /// <summary>
+        /// Moneda.
+        /// </summary>
+        public int Currency { get; set; }
+        /// <summary>
+        /// Descripción para moneda.
+        /// </summary>
+        public string CurrencyDesc { get; set; }
+        /// <summary>
+        /// Suma asegurada complementaria.
+        /// </summary>
+        public double Amount { get; set; }
+        public string Reference { get; set; }
+        public string Description { get; set; }
+        /// <summary>
+        /// Fecha de alta.
+        /// </summary>
+        public DateTime IssueDate { get; set; }
+        public DateTime StatusDate { get; set; }
+        public Int64 RequestID { get; set; }
+        public string ProcessUrl { get; set; }
+        public string ProviderStatus { get; set; }
+        public string ProviderStatusDesc { get; set; }
+        public string ResponseData { get; set; }
+
+        public string Reason { get; set; }
+
+
+        public string Receipt { get; set; }
+
+        public string Authorization { get; set; }
+
+        /// <summary>
+        /// Estado de la póliza.
+        /// </summary>
+        public int Status { get; set; }
+        /// <summary>
+        /// Descripción para estado de la póliza.
+        /// </summary>
+        public string StatusDesc { get; set; }
+        /// <summary>
+        /// Usuario que actualizo por última vez el registro.
+        /// </summary>
+        public int UpdateUserCode { get; set; }
+        /// <summary>
+        /// Nombre del usuario que actualizo por última vez el registro.
+        /// </summary>
+        public string UpdateUserName { get; set; }
+        /// <summary>
+        /// Fecha que se actualizo por última vez el registro.
+        /// </summary>
+        public DateTime UpdateDate { get; set; }
+    }
+}
+
