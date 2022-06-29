3bc2045395f6be8a885e2576722dcaf3cce73863
Nelson Soler <62022284+nesoler@users.noreply.github.com>
2022-06-29 17:07:54 -0600

    Prmite imprimir los certificado del ramo 205

--- Tron/Architect.API.Tron.Business/Backoffice/Common.cs
+++ Tron/Architect.API.Tron.Business/Backoffice/Common.cs
@@ -186,6 +186,9 @@ namespace Architect.API.Tron.Business.Backoffice
                 case "202":
                     procedureName = "em_k_jrp_cuadropoliza202_mcr.p_lista";
                     break;
+                case "205":
+                    procedureName = "em_k_jrp_certificado_205_mcr.p_lista";
+                    break;
                 case "288":
                     procedureName = "em_k_jrp_condiciones_288_mcr.p_lista";
                     break;
