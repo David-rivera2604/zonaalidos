93079e9d2140340051894f314b0b02b19792c732
nesoler <nsoler@grupoinmotion.com>
2022-08-30 05:53:43 -0600

    Viajero Regional Monge

--- Monge/Aliados.Monge.WebApi/Controllers/SeguridadController.cs
+++ Monge/Aliados.Monge.WebApi/Controllers/SeguridadController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.Web.Http;
-using System;
 using System.Collections.Generic;
-using System.Diagnostics.Contracts;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
@@ -38,17 +35,18 @@ namespace Aliados.Monge.WebApi.Controllers
             Request.Headers.TryGetValues("secretID", out values);
             if (values != null && values.Count() > 0)
                 secretID = values.FirstOrDefault();
- 
+
             Domain.Seguridad.RespuestaSeguridad x1 = Application.Seguridad.SeguridadHandler.Autorizacion(clienteID, secretID).Result;
 
-            if (x1!= null)
+            if (x1 != null)
             {
                 return Ok(x1);
-            } else
+            }
+            else
             {
                 return Unauthorized();
             }
-            
+
         }
     }
 }
