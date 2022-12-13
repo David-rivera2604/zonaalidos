0ce18dc9e494a77e4e9916c45d4909fcb8e0f1e9
Nelson Soler <62022284+nesoler@users.noreply.github.com>
2022-12-12 18:01:26 -0600

    Lista de profesiones. Validación de montos.

--- webapp/Scripts/Emision.kyc.persona.js
+++ webapp/Scripts/Emision.kyc.persona.js
@@ -7,7 +7,7 @@ app.kycpersona = (function () {
     let acceptCallback = null;
 
     function Setup(data) {
-        app.core.Lookups(['Pais.nacionalidadPer', 'Pais.paisdenacimientoPer', 'CivilStatus.estadocivilPer', 'Gender.sexoPer', 'Paises.cod_paisPer', 'Provincias.cod_estadoPer', 'Paises.domiciliocomercialCod_paisPer', 'Provincias.domiciliocomercialCod_estadoPer'],
+        app.core.Lookups(['Pais.nacionalidadPer', 'Pais.paisdenacimientoPer','TRON_G1000100.profesionPer', 'CivilStatus.estadocivilPer', 'Gender.sexoPer', 'Paises.cod_paisPer', 'Provincias.cod_estadoPer', 'Paises.domiciliocomercialCod_paisPer', 'Provincias.domiciliocomercialCod_estadoPer'],
             function () {
                 setupData = data;
                 MapObjectToInput(data);
@@ -189,7 +189,7 @@ app.kycpersona = (function () {
             decimalCharacter: ',',
             decimalCharacterAlternative: '.',
             digitGroupSeparator: '.',
-            maximumValue: '999999999',
+            maximumValue: '999999999999999999',
             minimumValue: '0',
             decimalPlaces: '2',
             emptyInputBehavior: 'null'
@@ -198,7 +198,7 @@ app.kycpersona = (function () {
             decimalCharacter: ',',
             decimalCharacterAlternative: '.',
             digitGroupSeparator: '.',
-            maximumValue: '999999999',
+            maximumValue: '999999999999999999',
             minimumValue: '0',
             decimalPlaces: '2',
             emptyInputBehavior: 'null'
@@ -207,7 +207,7 @@ app.kycpersona = (function () {
             decimalCharacter: ',',
             decimalCharacterAlternative: '.',
             digitGroupSeparator: '.',
-            maximumValue: '999999999',
+            maximumValue: '999999999999999999',
             minimumValue: '0',
             decimalPlaces: '2',
             emptyInputBehavior: 'null'
@@ -369,7 +369,7 @@ app.kycpersona = (function () {
         Init: function (data) {
             try {
                 Controls_setup();
-                Setup_Validations();
+                //Setup_Validations();
 
                 Controls_Events();
                 Setup(data);
