611bb6283a08d137032ae11d65c9d75dc0e2842f
Nelson Soler <solernelson@hotmail.com>
2023-03-06 14:31:01 -0600

    Envió de correo certificado para controles técnicos, Renovación de pólizas.

---
+++ dbscripts/230306.a.visualization.sql
@@ -0,0 +1,257 @@
+﻿SET DEFINE OFF;
+MERGE INTO VISUALIZATIONS A USING
+ (SELECT
+  341 as ID,
+  1 as SEQUENCE,
+  NULL as KEY,
+  1 as COMPANYID,
+  NULL as ENTITYTYPE,
+  1 as TYPE,
+  'Renovación de pólizas' as CAPTION,
+  'Renovación de pólizas' as DESCRIPTION,
+  2 as STATEMENTTYPE,
+  'EM_K_TABLES_CONTRACT_MCR.p_lee_polizas_cotz_13_meses {app.P_COD_CIA} {app.P_COD_AGT}' as STATEMENT,
+  '{
+    "title": "Renovación de pólizas",
+    "table": {
+        "pagination": true,
+        "search": true,
+        "showToggle": false,
+        "showRefresh": true,
+        "showColumns": false,
+        "showExport": true,
+        "showPaginationSwitch": false,
+        "exportTypes": [
+        "csv",
+        "excel"
+        ],
+        "columns": [
+            {
+                "field": "NUM_POLIZA",
+                "title": "No. Póliza",
+                "sortable": true,
+                "halign": "center",
+                "align": "center",
+                "formatter": "app.ui.StringFormatter",
+                "visible": true
+            },
+            {
+                "field": "ABR_RAMO",
+                "title": "Ramo",
+                "sortable": true,
+                "halign": "center",
+                "align": "center",
+                "formatter": "app.ui.StringFormatter",
+                "visible": true
+            },
+            {
+                "field": "NOM_COMPLETO",
+                "title": "Contratante",
+                "sortable": true,
+                "halign": "center",
+                "align": "left",
+                "format": "{IDENTIFICACION} {NOM_COMPLETO}",
+                "visible": true
+            },            
+            {
+                "field": "FEC_EFEC_POLIZA",
+                "title": "Inicio de vigencia",
+                "sortable": true,
+                "halign": "center",
+                "align": "center",
+                "formatter": "app.ui.DateFormatter",
+                "visible": true
+            },
+            {
+                "field": "FEC_VCTO_POLIZA",
+                "title": "Fin de vigencia",
+                "sortable": true,
+                "halign": "center",
+                "align": "center",
+                "formatter": "app.ui.DateFormatter",
+                "visible": true
+            },
+            {
+                "field": "NOM_RIESGO",
+                "title": "Riesgo",
+                "sortable": true,
+                "halign": "center",
+                "align": "left",
+                "formatter": "app.ui.StringFormatter",
+                "visible": true
+            },
+            {
+                "title": "Acciones",
+                "sortable": true,
+                "halign": "center",
+                "align": "center",
+                "formatter": "function (value, row, index, field) { return ''<span class=columnBtn>'' + ''<button type=\"button\" name=\"renovarPol\" class=\"btn btn-sm btn-white event\" title=\"Permite renovar una póliza\"><i class=\"fa fa-step-forward\"></i></button>''+''</span>'' ;}",
+                "visible": true,
+                "events": true,
+                "action_renovarPol": "app.ui.ShowSideBar({ title: ''Renovar la póliza'', subtitle: ''#{NUM_POLIZA}'', id: 9005, data: row, width: ''400px''})"
+            }
+        ]
+    }
+}' as SPECIFICATION,
+  NULL as DIALOG,
+  NULL as ICON,
+  NULL as ICONCLASS,
+  NULL as VALUEFORMAT,
+  'Tron' as CONNECTIONNAME,
+  'Extend.Policy.js' as JAVASCRIPTTOINCLUDE,
+  1 as RECORDSTATUS,
+  888 as UPDATEUSERCODE,
+  TO_DATE('03/31/2023 00:00:00', 'MM/DD/YYYY HH24:MI:SS') as UPDATEDATE
+  FROM DUAL) B
+ON (A.ID = B.ID and A.SEQUENCE = B.SEQUENCE)
+WHEN NOT MATCHED THEN 
+INSERT (
+  ID, SEQUENCE, KEY, COMPANYID, ENTITYTYPE, 
+  TYPE, CAPTION, DESCRIPTION, STATEMENTTYPE, STATEMENT, 
+  SPECIFICATION, DIALOG, ICON, ICONCLASS, VALUEFORMAT, 
+  CONNECTIONNAME, JAVASCRIPTTOINCLUDE, RECORDSTATUS, UPDATEUSERCODE, UPDATEDATE)
+VALUES (
+  B.ID, B.SEQUENCE, B.KEY, B.COMPANYID, B.ENTITYTYPE, 
+  B.TYPE, B.CAPTION, B.DESCRIPTION, B.STATEMENTTYPE, B.STATEMENT, 
+  B.SPECIFICATION, B.DIALOG, B.ICON, B.ICONCLASS, B.VALUEFORMAT, 
+  B.CONNECTIONNAME, B.JAVASCRIPTTOINCLUDE, B.RECORDSTATUS, B.UPDATEUSERCODE, B.UPDATEDATE)
+WHEN MATCHED THEN
+UPDATE SET 
+  A.KEY = B.KEY,
+  A.COMPANYID = B.COMPANYID,
+  A.ENTITYTYPE = B.ENTITYTYPE,
+  A.TYPE = B.TYPE,
+  A.CAPTION = B.CAPTION,
+  A.DESCRIPTION = B.DESCRIPTION,
+  A.STATEMENTTYPE = B.STATEMENTTYPE,
+  A.STATEMENT = B.STATEMENT,
+  A.SPECIFICATION = B.SPECIFICATION,
+  A.DIALOG = B.DIALOG,
+  A.ICON = B.ICON,
+  A.ICONCLASS = B.ICONCLASS,
+  A.VALUEFORMAT = B.VALUEFORMAT,
+  A.CONNECTIONNAME = B.CONNECTIONNAME,
+  A.JAVASCRIPTTOINCLUDE = B.JAVASCRIPTTOINCLUDE,
+  A.RECORDSTATUS = B.RECORDSTATUS,
+  A.UPDATEUSERCODE = B.UPDATEUSERCODE,
+  A.UPDATEDATE = B.UPDATEDATE;
+
+MERGE INTO VISUALIZATIONS A USING
+ (SELECT
+  9003 as ID,
+  1 as SEQUENCE,
+  NULL as KEY,
+  1 as COMPANYID,
+  NULL as ENTITYTYPE,
+  9 as TYPE,
+  'Cancelar una póliza' as CAPTION,
+  'Panel' as DESCRIPTION,
+  NULL as STATEMENTTYPE,
+  NULL as STATEMENT,
+  NULL as SPECIFICATION,
+  'Cancelar la póliza #|título
+|zona|12
+ Fecha de cancelación+|fecha|default=today;name=Fec_Anulacion
+ Motivo+|nota|filas=2;name=Mot_Spto
+ Cancelar|Boton|name=CancelarBtn;click=app.Ex_Policy.Cancelar({NUM_POLIZA});class=btn-danger' as DIALOG,
+  NULL as ICON,
+  NULL as ICONCLASS,
+  NULL as VALUEFORMAT,
+  NULL as CONNECTIONNAME,
+  NULL as JAVASCRIPTTOINCLUDE,
+  1 as RECORDSTATUS,
+  888 as UPDATEUSERCODE,
+  TO_DATE('03/31/2023 00:00:00', 'MM/DD/YYYY HH24:MI:SS') as UPDATEDATE
+  FROM DUAL) B
+ON (A.ID = B.ID and A.SEQUENCE = B.SEQUENCE)
+WHEN NOT MATCHED THEN 
+INSERT (
+  ID, SEQUENCE, KEY, COMPANYID, ENTITYTYPE, 
+  TYPE, CAPTION, DESCRIPTION, STATEMENTTYPE, STATEMENT, 
+  SPECIFICATION, DIALOG, ICON, ICONCLASS, VALUEFORMAT, 
+  CONNECTIONNAME, JAVASCRIPTTOINCLUDE, RECORDSTATUS, UPDATEUSERCODE, UPDATEDATE)
+VALUES (
+  B.ID, B.SEQUENCE, B.KEY, B.COMPANYID, B.ENTITYTYPE, 
+  B.TYPE, B.CAPTION, B.DESCRIPTION, B.STATEMENTTYPE, B.STATEMENT, 
+  B.SPECIFICATION, B.DIALOG, B.ICON, B.ICONCLASS, B.VALUEFORMAT, 
+  B.CONNECTIONNAME, B.JAVASCRIPTTOINCLUDE, B.RECORDSTATUS, B.UPDATEUSERCODE, B.UPDATEDATE)
+WHEN MATCHED THEN
+UPDATE SET 
+  A.KEY = B.KEY,
+  A.COMPANYID = B.COMPANYID,
+  A.ENTITYTYPE = B.ENTITYTYPE,
+  A.TYPE = B.TYPE,
+  A.CAPTION = B.CAPTION,
+  A.DESCRIPTION = B.DESCRIPTION,
+  A.STATEMENTTYPE = B.STATEMENTTYPE,
+  A.STATEMENT = B.STATEMENT,
+  A.SPECIFICATION = B.SPECIFICATION,
+  A.DIALOG = B.DIALOG,
+  A.ICON = B.ICON,
+  A.ICONCLASS = B.ICONCLASS,
+  A.VALUEFORMAT = B.VALUEFORMAT,
+  A.CONNECTIONNAME = B.CONNECTIONNAME,
+  A.JAVASCRIPTTOINCLUDE = B.JAVASCRIPTTOINCLUDE,
+  A.RECORDSTATUS = B.RECORDSTATUS,
+  A.UPDATEUSERCODE = B.UPDATEUSERCODE,
+  A.UPDATEDATE = B.UPDATEDATE;
+
+MERGE INTO VISUALIZATIONS A USING
+ (SELECT
+  9005 as ID,
+  1 as SEQUENCE,
+  NULL as KEY,
+  1 as COMPANYID,
+  NULL as ENTITYTYPE,
+  9 as TYPE,
+  'Renovar una póliza' as CAPTION,
+  'Panel' as DESCRIPTION,
+  NULL as STATEMENTTYPE,
+  NULL as STATEMENT,
+  NULL as SPECIFICATION,
+  'Renovar una póliza|título
+|zona|12
+ Renovar|Boton|event.clickWithValidate=app.Ex_Policy.Renovacion_Procesar;class=btn-primary' as DIALOG,
+  NULL as ICON,
+  NULL as ICONCLASS,
+  NULL as VALUEFORMAT,
+  NULL as CONNECTIONNAME,
+  NULL as JAVASCRIPTTOINCLUDE,
+  1 as RECORDSTATUS,
+  888 as UPDATEUSERCODE,
+  TO_DATE('03/31/2023 00:00:00', 'MM/DD/YYYY HH24:MI:SS') as UPDATEDATE
+  FROM DUAL) B
+ON (A.ID = B.ID and A.SEQUENCE = B.SEQUENCE)
+WHEN NOT MATCHED THEN 
+INSERT (
+  ID, SEQUENCE, KEY, COMPANYID, ENTITYTYPE, 
+  TYPE, CAPTION, DESCRIPTION, STATEMENTTYPE, STATEMENT, 
+  SPECIFICATION, DIALOG, ICON, ICONCLASS, VALUEFORMAT, 
+  CONNECTIONNAME, JAVASCRIPTTOINCLUDE, RECORDSTATUS, UPDATEUSERCODE, UPDATEDATE)
+VALUES (
+  B.ID, B.SEQUENCE, B.KEY, B.COMPANYID, B.ENTITYTYPE, 
+  B.TYPE, B.CAPTION, B.DESCRIPTION, B.STATEMENTTYPE, B.STATEMENT, 
+  B.SPECIFICATION, B.DIALOG, B.ICON, B.ICONCLASS, B.VALUEFORMAT, 
+  B.CONNECTIONNAME, B.JAVASCRIPTTOINCLUDE, B.RECORDSTATUS, B.UPDATEUSERCODE, B.UPDATEDATE)
+WHEN MATCHED THEN
+UPDATE SET 
+  A.KEY = B.KEY,
+  A.COMPANYID = B.COMPANYID,
+  A.ENTITYTYPE = B.ENTITYTYPE,
+  A.TYPE = B.TYPE,
+  A.CAPTION = B.CAPTION,
+  A.DESCRIPTION = B.DESCRIPTION,
+  A.STATEMENTTYPE = B.STATEMENTTYPE,
+  A.STATEMENT = B.STATEMENT,
+  A.SPECIFICATION = B.SPECIFICATION,
+  A.DIALOG = B.DIALOG,
+  A.ICON = B.ICON,
+  A.ICONCLASS = B.ICONCLASS,
+  A.VALUEFORMAT = B.VALUEFORMAT,
+  A.CONNECTIONNAME = B.CONNECTIONNAME,
+  A.JAVASCRIPTTOINCLUDE = B.JAVASCRIPTTOINCLUDE,
+  A.RECORDSTATUS = B.RECORDSTATUS,
+  A.UPDATEUSERCODE = B.UPDATEUSERCODE,
+  A.UPDATEDATE = B.UPDATEDATE;
+
+COMMIT;
---
+++ dbscripts/230306.b.template.sql
@@ -0,0 +1,94 @@
+﻿SET DEFINE OFF;
+MERGE INTO TEMPLATE A USING
+ (SELECT
+  500 as ID,
+  0 as COMPANYID,
+  'Send_Certificate' as KEY,
+  'Envio del certificado de una póliza emitida' as DESCRIPTION,
+  NULL as EMAILTO,
+  'Notificación de Certificado de Póliza' as SUBJECT,
+  '<p>Estimado(a) Asegurado(a),<br /><br /><br />Para <strong>MAPFRE Seguros Costa Rica</strong> es un placer atenderle en su póliza de seguro y al mismo tiempo agradecemos la confianza y preferencia que ha depositado en nosotros.<br /><br />Nos permitimos remitir el certificado de seguro correspondiente a su póliza número <strong>{Data.num_poliza}</strong> mismo que considera los términos y condiciones aplicables para el periodo en vigencia.<br /><br />Quedamos a su entera disposición para cualquier consulta. Contáctenos a través de los siguientes medios:<br /><br /></p>
+<ul>
+<li>Linea Gratuita 800 MAPFRE(8000 627373)</li>
+<li>Central Telefónica 4104-0200</li>
+<li><a href="mailto:servicioalcliente@mapfrecr.com" target="_blank" rel="noopener">servicioalcliente@mapfrecr.com</a></li>
+</ul>
+<p><br /><br />Estamos en la mejor disposición de brindarle el mejor servicio.<br /><br /></p>
+<center><strong>Atentamente,</strong></center><center><strong>MAPFRE Seguros Costa Rica</strong></center>' as BODY,
+  NULL as MASTERTEMPLATEID,
+  6329 as UPDATEUSERCODE,
+  TO_DATE('02/07/1970 00:00:00', 'MM/DD/YYYY HH24:MI:SS') as UPDATEDATE
+  FROM DUAL) B
+ON (A.ID = B.ID)
+WHEN NOT MATCHED THEN 
+INSERT (
+  ID, COMPANYID, KEY, DESCRIPTION, EMAILTO, 
+  SUBJECT, BODY, MASTERTEMPLATEID, UPDATEUSERCODE, UPDATEDATE)
+VALUES (
+  B.ID, B.COMPANYID, B.KEY, B.DESCRIPTION, B.EMAILTO, 
+  B.SUBJECT, B.BODY, B.MASTERTEMPLATEID, B.UPDATEUSERCODE, B.UPDATEDATE)
+WHEN MATCHED THEN
+UPDATE SET 
+  A.COMPANYID = B.COMPANYID,
+  A.KEY = B.KEY,
+  A.DESCRIPTION = B.DESCRIPTION,
+  A.EMAILTO = B.EMAILTO,
+  A.SUBJECT = B.SUBJECT,
+  A.BODY = B.BODY,
+  A.MASTERTEMPLATEID = B.MASTERTEMPLATEID,
+  A.UPDATEUSERCODE = B.UPDATEUSERCODE,
+  A.UPDATEDATE = B.UPDATEDATE;
+
+MERGE INTO TEMPLATE A USING
+ (SELECT
+  2007 as ID,
+  2 as COMPANYID,
+  'Control_Tecnico' as KEY,
+  'Envia de observaciones sobre controles técnicos' as DESCRIPTION,
+  NULL as EMAILTO,
+  'Notificación control técnico' as SUBJECT,
+  '<font face=''tahoma'' size=2>Estimado(a) Usuario(a),</font>
+<br />
+<br />
+<br />
+
+<font face=''tahoma'' size=2>Nos permitimos informarle que para la póliza: </font><font face=''tahoma'' size=2><b>{Data.num_poliza}</b></font><font face=''tahoma'' size=2></font>
+<br />
+<br />
+<font face=''tahoma'' size=2>Se encuentra en un estatus de pendiente de procesar por el siguiente motivo: {Data.observacion}</font>
+<br />
+<br />
+
+<br />
+<br />
+<font face=''tahoma'' size=2>Estamos en la mejor disposición de brindarle el mejor servicio.</font>
+<br />
+<br />
+<center><font face=''tahoma'' size=2><b>Atentamente,</b></font></center>
+<center><font face=''tahoma'' size=2><b>MAPFRE Seguros Costa Rica</b></font></center>
+' as BODY,
+  0 as MASTERTEMPLATEID,
+  254 as UPDATEUSERCODE,
+  TO_DATE('03/06/2023 13:17:57', 'MM/DD/YYYY HH24:MI:SS') as UPDATEDATE
+  FROM DUAL) B
+ON (A.ID = B.ID)
+WHEN NOT MATCHED THEN 
+INSERT (
+  ID, COMPANYID, KEY, DESCRIPTION, EMAILTO, 
+  SUBJECT, BODY, MASTERTEMPLATEID, UPDATEUSERCODE, UPDATEDATE)
+VALUES (
+  B.ID, B.COMPANYID, B.KEY, B.DESCRIPTION, B.EMAILTO, 
+  B.SUBJECT, B.BODY, B.MASTERTEMPLATEID, B.UPDATEUSERCODE, B.UPDATEDATE)
+WHEN MATCHED THEN
+UPDATE SET 
+  A.COMPANYID = B.COMPANYID,
+  A.KEY = B.KEY,
+  A.DESCRIPTION = B.DESCRIPTION,
+  A.EMAILTO = B.EMAILTO,
+  A.SUBJECT = B.SUBJECT,
+  A.BODY = B.BODY,
+  A.MASTERTEMPLATEID = B.MASTERTEMPLATEID,
+  A.UPDATEUSERCODE = B.UPDATEUSERCODE,
+  A.UPDATEDATE = B.UPDATEDATE;
+
+COMMIT;
