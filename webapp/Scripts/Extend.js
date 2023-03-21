bdc5599620c952cabc9854a0cbda2363df2a6499
Nelson Soler <62022284+nesoler@users.noreply.github.com>
2023-03-20 23:26:07 -0600

    1.6.26

--- webapp/Scripts/Extend.js
+++ webapp/Scripts/Extend.js
@@ -20,6 +20,26 @@ app.Extend = (function () {
                 + '?mode=' + mode
                 + '&presupuesto=' + row.NUM_PRESUPUESTO + '>' + value + '</a>' : row.NUM_PRESUPUESTO;
         },
+        Filter345handler: function (custom, data) {
+             if (data.p_num_contrato == 0 &&
+                data.p_num_poliza_grupo == 0 &&
+                data.p_num_poliza == '') {
+                toastr.error('Debe indicar por lo menos el valor para unos de los campos', '', { timeOut: 7000, closeButton: true, progressBar: true });
+            } else {
+
+                if (data.p_num_contrato == 0) {
+                    data.p_num_contrato = null;
+                }
+                if (data.p_num_poliza_grupo == 0) {
+                    data.p_num_poliza_grupo = null;
+                }
+                app.ui.ButtonDoing('#filtrar');
+                app.ViewerQuery.Refresh(undefined, $('#1GridTbl'), 345, '', 1, data,
+                    function () {
+                        app.ui.ButtonDone('#filtrar');
+                    });
+            }
+        },
         EventHandler: function (id, index, stage) {
             if (id == '310' && index == 2 && stage == 'loaded') {
                 $("#QueryFNotify2").html('<div class="row d-none role-Pago-visible" style="padding-top: 25px;"><div class="col-9"><a href="javascript:app.master.ShowSideBarExternal(\'Preguntas y respuestas frecuentes\', \'/aliados/viewer/render?id=392&t=4&wd=400px\');" style="font-size: smaller;">Ver preguntas y respuestas frecuentes sobre pagos electrónicos</a></div><div class="col-3"><a href="https://www.placetopay.com/web/" target="_blank"><img src="https://static.placetopay.com/placetopay-logo.svg" class="img-fluid float-right" alt="Responsive image" style="width: 125px;"></a></div></div>')
