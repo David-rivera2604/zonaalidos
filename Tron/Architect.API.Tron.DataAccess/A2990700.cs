f2803aafe0a2b1f607ec72a408b5c990e38220d4
Nelson Soler <62022284+nesoler@users.noreply.github.com>
2023-05-25 19:16:49 -0600

    Pagos. La consulta de recibos pendiente debe validar si existe un recibo remesado.

---
+++ Tron/Architect.API.Tron.DataAccess/A2990700.cs
@@ -0,0 +1,24 @@
+﻿using Architect.DataFactory;
+using System;
+using System.Data;
+using DbType = Architect.DataFactory.Enumerations.DbType;
+
+namespace Architect.API.Tron.DataAccess
+{
+    /// <summary>
+    /// RECIBOS/CUOTAS DE UNA POLIZA.
+    /// </summary>
+    public static class A2990700
+    {
+
+        public static int RecibosRemesadosPorPoliza(int cod_cia, string num_poliza, IDbConnection connection = null)
+        {
+            return (int)Database.Select("SELECT COUNT(NUM_RECIBO) " +
+                                          "FROM A2990700 " +
+                                         "WHERE COD_CIA=:COD_CIA AND NUM_POLIZA=:NUM_POLIZA and TIP_SITUACION='RE'")
+                        .AddParameter("COD_CIA", DbType.Decimal, 5, cod_cia)
+                        .AddParameter("NUM_POLIZA", DbType.AnsiString, 13, num_poliza)
+                        .QueryScalar<Decimal>(connection, "Tron");
+        }
+    }
+}
