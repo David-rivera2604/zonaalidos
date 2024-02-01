47b023acdcf8d2a857a6a25adbbbe16a5b8493df
roynera <royner.acosta@mapfrecr.com>
2024-02-01 10:33:28 -0600

    Tron. Api Imprimir poliza en Base64. Rules. Mapfremas y plus se ajustan las reglas segun lo que hay en prd Coopeservidores. Se agrega validacion de pedir obligatorio los adjuntos en la emision

---
+++ Tron/Architect.API.Tron.Contracts/Comun/Certificado.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Architect.API.Tron.Contracts.Comun
+{
+    /// <summary>
+    /// Certificado de Poliza.
+    /// </summary>
+    public class Certificado
+    {
+        /// <summary>
+        /// Nombre.
+        /// </summary>
+        [DataMember(), JsonProperty()] public string certificado { get; set; }
+    }
+}
