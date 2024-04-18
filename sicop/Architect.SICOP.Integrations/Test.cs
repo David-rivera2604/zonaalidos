11c1938e2eaf7b30c70e125617814b9c3b20abb3
Nelson Soler <solernelson@hotmail.com>
2024-04-18 13:05:45 -0600

    SICOP base

---
+++ sicop/Architect.SICOP.Integrations/Test.cs
@@ -0,0 +1,53 @@
+﻿using Architect.SICOP.Integrations.SupplierInfoWS;
+using Architect.Utilities.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Architect.SICOP.Integrations
+{
+    public class Test
+    {
+
+        static public Contracts.SUPPLIER_INFO SupplierInfo(string supplier_identifier)
+        {
+            getSupplierInfoResponse result = null;
+            Contracts.SUPPLIER_INFO info = new Contracts.SUPPLIER_INFO()
+            {
+                Confirmation = "3",
+                Msg_err = "Ha ocurrido un error al intentar consultar la información del proveedor"
+            };
+
+            using (SupplierInfoWServiceClient client = new SupplierInfoWServiceClient())
+            {
+                result = client.getSupplierInfo(new getSupplierInfoRequest()
+                {
+                    Body = new getSupplierInfoRequestBody() { supplier_identifier = supplier_identifier }
+                });
+                client.Close();
+            }
+
+            if (result.IsNotEmpty() && result.Body.IsNotEmpty() && result.Body.@return.IsNotEmpty())
+            {
+                info = Utilities.SerializeHandler<Contracts.SUPPLIER_INFO>.Deserialize(result.Body.@return);
+
+                switch (info.Confirmation)
+                {
+                    case "3":
+                        info.Msg_err = "Ha ocurrido un error al intentar consultar la información del proveedor";
+                        break;
+                    case "5":
+                        info.Msg_err = $"No existe información registra del proveedor con número de identificación {supplier_identifier}";
+                        break;
+                }
+            }
+
+            return info;
+        }
+    }
+}
