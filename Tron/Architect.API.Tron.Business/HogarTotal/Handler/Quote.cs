1de7234cedc440db2f63a109d8b57584aa35eabe
roynera <royner.acosta@mapfrecr.com>
2024-09-20 09:49:44 -0600

    HogarTotal - Agrupamiento de coberturas. Se quita la cobertura 2014 de la lista

--- Tron/Architect.API.Tron.Business/HogarTotal/Handler/Quote.cs
+++ Tron/Architect.API.Tron.Business/HogarTotal/Handler/Quote.cs
@@ -115,7 +115,7 @@ namespace Architect.API.Tron.Business.HogarTotal.Handler
             {
                 if (isCoope)
                 {
-                    cod_cobExcludeFilter = "2007, 2017,2018, 2034, 2010, 2055, 2056, 2057, 2012, 2014";
+                    cod_cobExcludeFilter = "2007, 2017,2018, 2034, 2010, 2055, 2056, 2057, 2012";
                 }
                 else
                 {
