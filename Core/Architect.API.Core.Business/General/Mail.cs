563639e948fec11f45b54c7cbc7841e63278e7f5
Nelson Soler <nsoler@grupoinmotion.com>
2024-12-03 14:16:46 -0600

    Se agregar la posibilidad de configurar el email de pruebas desde la tabla settings

--- Core/Architect.API.Core.Business/General/Mail.cs
+++ Core/Architect.API.Core.Business/General/Mail.cs
@@ -92,7 +92,7 @@ namespace Architect.API.Core.Business.General
             Architect.API.Core.Contracts.Security.UserMember currentUserInfo = new Contracts.Security.UserMember();
             Architect.API.Core.Contracts.Security.UserMember ownerUserInfo = new Contracts.Security.UserMember();
             Core.Contracts.General.Tenant tenantInfo = new Contracts.General.Tenant();
-            string testEmail = ConfigurationManager.AppSettings["EMail.Test"];
+            string testEmail = Business.Settings.StringValue("EMail.Test");
 
             if (toAddressList == null)
             {
