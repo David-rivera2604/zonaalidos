886bb22aa7d362a7f81faf1437aa6ee5bdf0b42a
Nelson Soler <nsoler@grupoinmotion.com>
2025-03-18 13:39:45 -0600

    SINPEMovil

--- Tron/Architect.API.Tron.Business/Backoffice/SINPEMovil.cs
+++ Tron/Architect.API.Tron.Business/Backoffice/SINPEMovil.cs
@@ -19,185 +19,250 @@ using Newtonsoft.Json;
 namespace Architect.API.Tron.Business.Backoffice
 {
     /// <summary>
-    /// 
+    /// Clase para manejar las operaciones relacionadas con SINPE Movil.
     /// </summary>
     public class SINPEMovil
     {
+        private const int PaymentSuccessful = 0;
+        private const int PaymentNotRegistered = 1;
+        private const int PaymentPartial = 2;
+        private const int ErrorInProcess = 99;
+        private const int RegisteredButNotProcessed = 98;
+
+        /// <summary>
+        /// Crea una nueva solicitud de pago SINPE Movil.
+        /// </summary>
+        /// <param name="aplicarPagoRequest">La solicitud de pago a aplicar.</param>
+        /// <returns>El resultado de la aplicación del pago.</returns>
         public async static Task<Contracts.SINPEMovil.Response.AplicarPago> Create(Contracts.SINPEMovil.Request.AplicarPago aplicarPagoRequest)
         {
-
-            /// Código que identifica el resultado del proceso.
-            /// 0: Pago realizado exitosamente.
-            /// 1: Pago no registrado.
-            /// 2: Pago parcial.
-            /// 99: Error en el proceso.
             int id = 0;
-            Contracts.SINPEMovil.Response.AplicarPago result = new Contracts.SINPEMovil.Response.AplicarPago()
+            var result = new Contracts.SINPEMovil.Response.AplicarPago
             {
-                Codigo = 98,
+                Codigo = RegisteredButNotProcessed,
                 Mensaje = "Registrado pero no procesado."
             };
 
-            using (DataFactory.Session session = new DataFactory.Session("Research"))
+            using (var session = new DataFactory.Session("Research"))
             {
                 try
                 {
-                    Contracts.SINPEMovil.Request.AplicarPago current = DataAccess.Pagos.SINPEMovil.RetrieveByCodReferencia(aplicarPagoRequest.CodReferencia, session);
-                    id = current.Id;
-                    if (id == 0)
-                    {
-                        aplicarPagoRequest.Estado = result.Codigo;
-                        id = DataAccess.Pagos.SINPEMovil.Create(aplicarPagoRequest, JsonConvert.SerializeObject(aplicarPagoRequest.DatosAdicionales), session);
-                        if (id > 0)
-                        {
-                            Core.Business.General.ChangeSet.Create(3001, id, 1, "Creación", string.Format("Se registro el SINPE Movil '{0}' de {1} - {2} ({3})", aplicarPagoRequest.TelefonoDestino, aplicarPagoRequest.IdentificacionCliente, aplicarPagoRequest.NombreCliente, aplicarPagoRequest.CodReferencia), 1, aplicarPagoRequest);
-                        }
-                    }
-                    else
-                    {
-                        if (current.Estado != 0)
-                        {
-                            int rows = DataAccess.Pagos.SINPEMovil.Update(id, aplicarPagoRequest, JsonConvert.SerializeObject(aplicarPagoRequest.DatosAdicionales), session);
-                            if (rows > 0)
-                            {
-                                Core.Business.General.ChangeSet.Create(3001, id, 1, "Modificación", string.Format("Se actualizo el SINPE Movil '{0}' de {1} - {2} ({3})", aplicarPagoRequest.TelefonoDestino, aplicarPagoRequest.IdentificacionCliente, aplicarPagoRequest.NombreCliente, aplicarPagoRequest.CodReferencia), 1, aplicarPagoRequest);
-                            }
-                        }
-                        else
-                        {
-                            Core.Business.General.ChangeSet.Create(3001, id, 1, "Modificación", string.Format("Se ignoró el SINPE Movil '{0}', ya fue aplicado de forma exitosa ({3})", aplicarPagoRequest.TelefonoDestino, aplicarPagoRequest.IdentificacionCliente, aplicarPagoRequest.NombreCliente, aplicarPagoRequest.CodReferencia), 1, aplicarPagoRequest);
-
-                            //Notificación que el pago ya fue realizado.
-
-                        }
-                    }
+                    id = await ProcessPaymentRequest(aplicarPagoRequest, result, session);
                     session.CommitAndClose();
                 }
                 catch (Exception ex)
                 {
                     session.RollbackAndClose();
-                    throw ex;
+                    // Log the exception
+                    throw;
                 }
             }
 
-            //Se trata de aplicar el pago
-            if (id > 0 && result.Codigo == 98)
+            if (id > 0 && result.Codigo == RegisteredButNotProcessed)
             {
-                Contracts.Pagos.Recibo recibo = null;
-                string policyNumber = aplicarPagoRequest.Descripcion.OnlyNumbers();
-
-                if (policyNumber.Length != 13)
-                {
-                    result.Codigo = 99;
-                    result.Mensaje = "Error en el proceso. No se puede identificar el número de la póliza.";
-                }
-                else
-                {
-                    recibo = DataAccess.Pagos.Recibos.PrimerReciboAlCobroPorPoliza(policyNumber);
-                }
-                if (result.Codigo == 98 && recibo == null)
-                {
-                    result.Codigo = 99;
-                    result.Mensaje = "Error en el proceso. No existen recibos pendiente para la póliza.";
-                }
-                if (result.Codigo == 98 && aplicarPagoRequest.Monto != recibo.IMP_RECIBO)
-                {
-                    result.Codigo = 99;
-                    result.Mensaje = "Error en el proceso. El monto del recibo no coinciden con el pago.";
-                }
-                if (result.Codigo == 98 && (aplicarPagoRequest.Moneda != 1 || recibo.NOM_MON != "CRC"))
-                {
-                    result.Codigo = 99;
-                    result.Mensaje = "Error en el proceso. La moneda del recibo no coinciden con la del pago.";
-                }
-                if (result.Codigo == 98)
-                {
-
-
-                    OnlinePayment payment = new OnlinePayment()
-                    {
-                        CompanyId = 2,
-                        DocumentType = Architect.Payment.Integrations.Payment.IdentificationTypeConvert(recibo.TIP_DOCUM),
-                        DocumentNumber = recibo.COD_DOCUM,
-                        FirstName = recibo.NOM_TERCERO,
-                        LastName = recibo.APE1_TERCERO,
-                        PrimaryEmailAddress = recibo.EMAIL,
-                        PhoneNumberMobile = recibo.TLF_NUMERO,
-                        AgentCode = recibo.COD_AGT,
-                        PolicyId = recibo.NUM_POLIZA,
-                        BillNumber = recibo.NUM_RECIBO,
-                        Currency = Architect.Payment.Integrations.Payment.CurrencyConvert(recibo.NOM_MON),
-                        Amount = recibo.IMP_RECIBO,
-                        Reference = string.Format("{0}-{1}", recibo.NUM_POLIZA, recibo.NUM_RECIBO),
-                        Description = string.Format("MAPFRE: {3}. {0}. POLIZA #{1} RECIBO #{2}", recibo.NOM_RAMO, recibo.NUM_POLIZA, recibo.NUM_RECIBO, recibo.NOM_SECTOR),
-                        IssueDate = DateTime.Now,
-                        StatusDate = DateTime.Now,
-                        Status = 3,
-                        ProviderStatus = "PENDING",
-                        RecurringReceipt = true,
-                        ProcessId = id.ToString(),
-                        RequestID = recibo.NUM_RECIBO
-                    };
-
-                    OnlinePayment track = Payment.Integrations.Business.OnlinePayment.Create(2, 1, payment);
-
-                    InformationRequest infoReq = new InformationRequest()
-                    {
-                        status = "PENDING",
-                        reason = "Pending approval",
-                        description = track.Description,
-                        reference = track.Reference,
-                        currency = recibo.NOM_MON,
-                        total = track.Amount,
-                        message = "Payment is pending",
-                        payerName = track.FirstName,
-                        payerSurname = track.LastName,
-                        paymentMethodName = "SINPE",
-                        lastDigits = "0000",
-                        authorization = aplicarPagoRequest.CodReferencia,
-                        receipt = aplicarPagoRequest.CodReferencia,
-                        subscribe = false,
-                        date = payment.IssueDate.ToString("yyyy-MM-dd HH:mm:ss"),
-                        OnlinePayment = payment
-                    };
-                    string pagador = recibo.TIP_DOCUM + "-" + recibo.COD_DOCUM.DocumentNumber(recibo.TIP_DOCUM);
-                    bool paymentApplayed = await Pagos.TronPayment(infoReq, infoReq.OnlinePayment.AgentCode, "Placetopay", string.Empty, pagador);
-
-                    if (paymentApplayed)
-                    {
-                        result.Codigo = 0;
-                        result.Mensaje = "Pago realizado exitosamente.";
-                    } else
-                    {
-                        result.Codigo = 1;
-                        result.Mensaje = "Pago no registrado.";
-                    }
-
-                }
-
-
+                await ApplyPayment(aplicarPagoRequest, result, id);
             }
+
             if (id > 0)
             {
-                //result.Codigo = 0;
-                //result.Mensaje = "Pago realizado exitosamente.";
-                using (DataFactory.Session session = new DataFactory.Session("Research"))
-                {
-                    try
-                    {
-                        DataAccess.Pagos.SINPEMovil.UpdateEstado(id, result.Codigo, result.Mensaje, session);
-                        session.CommitAndClose();
-                    }
-                    catch (Exception ex)
-                    {
-                        session.RollbackAndClose();
-                        throw ex;
-                    }
-
-                }
+                UpdatePaymentStatus(id, result);
             }
 
             return result;
         }
+
+        /// <summary>
+        /// Procesa la solicitud de pago.
+        /// </summary>
+        /// <param name="aplicarPagoRequest">La solicitud de pago a aplicar.</param>
+        /// <param name="result">El resultado de la aplicación del pago.</param>
+        /// <param name="session">La sesión de base de datos.</param>
+        /// <returns>El ID del pago procesado.</returns>
+        private static async Task<int> ProcessPaymentRequest(Contracts.SINPEMovil.Request.AplicarPago aplicarPagoRequest, Contracts.SINPEMovil.Response.AplicarPago result, DataFactory.Session session)
+        {
+            int id;
+            var current = DataAccess.Pagos.SINPEMovil.RetrieveByCodReferencia(aplicarPagoRequest.CodReferencia, session);
+            id = current.Id;
+            if (id == 0)
+            {
+                aplicarPagoRequest.Estado = result.Codigo;
+                id = DataAccess.Pagos.SINPEMovil.Create(aplicarPagoRequest, JsonConvert.SerializeObject(aplicarPagoRequest.DatosAdicionales), session);
+                if (id > 0)
+                {
+                    Core.Business.General.ChangeSet.Create(3001, id, 1, "Creación", $"Se registro el SINPE Movil '{aplicarPagoRequest.TelefonoDestino}' de {aplicarPagoRequest.IdentificacionCliente} - {aplicarPagoRequest.NombreCliente} ({aplicarPagoRequest.CodReferencia})", 1, aplicarPagoRequest);
+                }
+            }
+            else
+            {
+                await HandleExistingPaymentRequest(aplicarPagoRequest, result, session, current, id);
+            }
+
+            return id;
+        }
+
+        /// <summary>
+        /// Maneja una solicitud de pago existente.
+        /// </summary>
+        /// <param name="aplicarPagoRequest">La solicitud de pago a aplicar.</param>
+        /// <param name="result">El resultado de la aplicación del pago.</param>
+        /// <param name="session">La sesión de base de datos.</param>
+        /// <param name="current">La solicitud de pago actual.</param>
+        /// <param name="id">El ID del pago procesado.</param>
+        private static async Task HandleExistingPaymentRequest(Contracts.SINPEMovil.Request.AplicarPago aplicarPagoRequest, Contracts.SINPEMovil.Response.AplicarPago result, DataFactory.Session session, Contracts.SINPEMovil.Request.AplicarPago current, int id)
+        {
+            if (current.Estado != PaymentSuccessful)
+            {
+                int rows = DataAccess.Pagos.SINPEMovil.Update(id, aplicarPagoRequest, JsonConvert.SerializeObject(aplicarPagoRequest.DatosAdicionales), session);
+                if (rows > 0)
+                {
+                    Core.Business.General.ChangeSet.Create(3001, id, 1, "Modificación", $"Se actualizo el SINPE Movil '{aplicarPagoRequest.TelefonoDestino}' de {aplicarPagoRequest.IdentificacionCliente} - {aplicarPagoRequest.NombreCliente} ({aplicarPagoRequest.CodReferencia})", 1, aplicarPagoRequest);
+                }
+            }
+            else
+            {
+                Core.Business.General.ChangeSet.Create(3001, id, 1, "Modificación", $"Se ignoró el SINPE Movil '{aplicarPagoRequest.TelefonoDestino}', ya fue aplicado de forma exitosa ({aplicarPagoRequest.CodReferencia})", 1, aplicarPagoRequest);
+                // Notificación que el pago ya fue realizado.
+            }
+        }
+
+        /// <summary>
+        /// Aplica el pago a la solicitud.
+        /// </summary>
+        /// <param name="aplicarPagoRequest">La solicitud de pago a aplicar.</param>
+        /// <param name="result">El resultado de la aplicación del pago.</param>
+        /// <param name="id">El ID del pago procesado.</param>
+        private static async Task ApplyPayment(Contracts.SINPEMovil.Request.AplicarPago aplicarPagoRequest, Contracts.SINPEMovil.Response.AplicarPago result, int id)
+        {
+            Contracts.Pagos.Recibo recibo = null;
+            string policyNumber = aplicarPagoRequest.Descripcion.OnlyNumbers();
+
+            if (policyNumber.Length != 13)
+            {
+                result.Codigo = ErrorInProcess;
+                result.Mensaje = "Error en el proceso. No se puede identificar el número de la póliza.";
+            }
+            else
+            {
+                recibo = DataAccess.Pagos.Recibos.PrimerReciboAlCobroPorPoliza(policyNumber);
+            }
+
+            if (result.Codigo == RegisteredButNotProcessed && recibo == null)
+            {
+                result.Codigo = ErrorInProcess;
+                result.Mensaje = "Error en el proceso. No existen recibos pendiente para la póliza.";
+            }
+
+            if (result.Codigo == RegisteredButNotProcessed && aplicarPagoRequest.Monto != recibo.IMP_RECIBO)
+            {
+                result.Codigo = ErrorInProcess;
+                result.Mensaje = "Error en el proceso. El monto del recibo no coinciden con el pago.";
+            }
+
+            if (result.Codigo == RegisteredButNotProcessed && (aplicarPagoRequest.Moneda != 1 || recibo.NOM_MON != "CRC"))
+            {
+                result.Codigo = ErrorInProcess;
+                result.Mensaje = "Error en el proceso. La moneda del recibo no coinciden con la del pago.";
+            }
+
+            if (result.Codigo == RegisteredButNotProcessed)
+            {
+                await ProcessOnlinePayment(aplicarPagoRequest, result, id, recibo);
+            }
+        }
+
+        /// <summary>
+        /// Procesa el pago en línea.
+        /// </summary>
+        /// <param name="aplicarPagoRequest">La solicitud de pago a aplicar.</param>
+        /// <param name="result">El resultado de la aplicación del pago.</param>
+        /// <param name="id">El ID del pago procesado.</param>
+        /// <param name="recibo">El recibo del pago.</param>
+        private static async Task ProcessOnlinePayment(Contracts.SINPEMovil.Request.AplicarPago aplicarPagoRequest, Contracts.SINPEMovil.Response.AplicarPago result, int id, Contracts.Pagos.Recibo recibo)
+        {
+            var payment = new OnlinePayment
+            {
+                CompanyId = 2,
+                DocumentType = Architect.Payment.Integrations.Payment.IdentificationTypeConvert(recibo.TIP_DOCUM),
+                DocumentNumber = recibo.COD_DOCUM,
+                FirstName = recibo.NOM_TERCERO,
+                LastName = recibo.APE1_TERCERO,
+                PrimaryEmailAddress = recibo.EMAIL,
+                PhoneNumberMobile = recibo.TLF_NUMERO,
+                AgentCode = recibo.COD_AGT,
+                PolicyId = recibo.NUM_POLIZA,
+                BillNumber = recibo.NUM_RECIBO,
+                Currency = Architect.Payment.Integrations.Payment.CurrencyConvert(recibo.NOM_MON),
+                Amount = recibo.IMP_RECIBO,
+                Reference = $"{recibo.NUM_POLIZA}-{recibo.NUM_RECIBO}",
+                Description = $"MAPFRE: {recibo.NOM_SECTOR}. {recibo.NOM_RAMO}. POLIZA #{recibo.NUM_POLIZA} RECIBO #{recibo.NUM_RECIBO}",
+                IssueDate = DateTime.Now,
+                StatusDate = DateTime.Now,
+                Status = 3,
+                ProviderStatus = "PENDING",
+                RecurringReceipt = true,
+                ProcessId = id.ToString(),
+                RequestID = recibo.NUM_RECIBO
+            };
+
+            var track = Payment.Integrations.Business.OnlinePayment.Create(2, 1, payment);
+
+            var infoReq = new InformationRequest
+            {
+                status = "PENDING",
+                reason = "Pending approval",
+                description = track.Description,
+                reference = track.Reference,
+                currency = recibo.NOM_MON,
+                total = track.Amount,
+                message = "Payment is pending",
+                payerName = track.FirstName,
+                payerSurname = track.LastName,
+                paymentMethodName = "SINPE",
+                lastDigits = "0000",
+                authorization = aplicarPagoRequest.CodReferencia,
+                receipt = aplicarPagoRequest.CodReferencia,
+                subscribe = false,
+                date = payment.IssueDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                OnlinePayment = payment
+            };
+
+            string pagador = $"{recibo.TIP_DOCUM}-{recibo.COD_DOCUM.DocumentNumber(recibo.TIP_DOCUM)}";
+            bool paymentApplied = await Pagos.TronPayment(infoReq, infoReq.OnlinePayment.AgentCode, "Placetopay", string.Empty, pagador);
+
+            if (paymentApplied)
+            {
+                result.Codigo = PaymentSuccessful;
+                result.Mensaje = "Pago realizado exitosamente.";
+            }
+            else
+            {
+                result.Codigo = PaymentNotRegistered;
+                result.Mensaje = "Pago no registrado.";
+            }
+        }
+
+        /// <summary>
+        /// Actualiza el estado del pago en la base de datos.
+        /// </summary>
+        /// <param name="id">El ID del pago procesado.</param>
+        /// <param name="result">El resultado de la aplicación del pago.</param>
+        private static void UpdatePaymentStatus(int id, Contracts.SINPEMovil.Response.AplicarPago result)
+        {
+            using (var session = new DataFactory.Session("Research"))
+            {
+                try
+                {
+                    DataAccess.Pagos.SINPEMovil.UpdateEstado(id, result.Codigo, result.Mensaje, session);
+                    session.CommitAndClose();
+                }
+                catch (Exception ex)
+                {
+                    session.RollbackAndClose();
+                    // Log the exception
+                    throw;
+                }
+            }
+        }
+    
     }
 }
