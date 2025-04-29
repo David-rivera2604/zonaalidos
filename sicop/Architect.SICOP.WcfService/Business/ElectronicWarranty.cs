381033914e92e1f16f146be23872cc9050772c5b
Nelson Soler <nsoler@grupoinmotion.com>
2025-04-29 14:52:43 -0600

    1.6.123 29/04/2025 releaseletter.txt

--- sicop/Architect.SICOP.WcfService/Business/ElectronicWarranty.cs
+++ sicop/Architect.SICOP.WcfService/Business/ElectronicWarranty.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,57 +12,70 @@ namespace Architect.SICOP.WcfService.Business
         {
             Contracts.ExecGurtReponse result = new Contracts.ExecGurtReponse() { Confirmation = "0", Msg_err = string.Empty };
 
-            if (value != null && !string.IsNullOrEmpty(value.Guarantee_Number) && !string.IsNullOrEmpty(value.Guarantee_sequencenumber))
+            try
             {
-                Contracts.ElectronicWarranty warranty = DataAccess.ElectronicWarranty.RetrieveByGuaranteeNumber(value.Guarantee_Number);
+                if (value != null && !string.IsNullOrEmpty(value.Guarantee_Number) && !string.IsNullOrEmpty(value.Guarantee_sequencenumber))
+                {
+                    Utilities.Log.TraceLog("Ejecutar_O_Liberar request", JsonConvert.SerializeObject(value), "ElectronicWarranty");
 
-                if (warranty == null)
-                {
-                    result.Confirmation = "10";
-                    result.Msg_err = "La garantía no existe";
-                }
-                if (result.Confirmation == "0" && warranty.Codigo_Proceso != string.Empty && warranty.Codigo_Proceso == value.codigo_proceso)
-                {
-                    result.Confirmation = "21";
-                    result.Msg_err = "La información enviada ya se encuentra procesada";
-                }
-                if (result.Confirmation == "0" && warranty.Codigo_Proceso != string.Empty)
-                {
-                    result.Confirmation = "25";
-                    result.Msg_err = string.Format("La garantía ya fue ejecutada o liberada con el código del proceso #{0}", warranty.Codigo_Proceso);
-                }
-                if (result.Confirmation == "0" && warranty.Guarantee_sequencenumber != value.Guarantee_sequencenumber)
-                {
-                    result.Confirmation = "22";
-                    result.Msg_err = "Sólo se pueden liberar/ejecutar garantías con la última secuencia";
-                }
-                if (result.Confirmation == "0" && warranty.Moneda != value.Moneda)
-                {
-                    result.Confirmation = "24";
-                    result.Msg_err = "Tipo de moneda no corresponde a la original";
-                }
+                    Contracts.ElectronicWarranty warranty = DataAccess.ElectronicWarranty.RetrieveByGuaranteeNumber(value.Guarantee_Number);
 
-                if (result.Confirmation == "0")
-                {
-                    if (value.Metodo == "0")
-                        value.MetodoDesc = "ejecutar";
-                    if (value.Metodo == "1")
-                        value.MetodoDesc = "liberar";
-                    DataAccess.ElectronicWarranty.Update(warranty.ID, value.Metodo, value.codigo_proceso, value.Amount, value.Contents, value.cuenta_Cliente);
+                    if (warranty == null)
+                    {
+                        result.Confirmation = "10";
+                        result.Msg_err = "La garantía no existe";
+                    }
+                    if (result.Confirmation == "0" && warranty.Codigo_Proceso != string.Empty && warranty.Codigo_Proceso == value.codigo_proceso)
+                    {
+                        result.Confirmation = "21";
+                        result.Msg_err = "La información enviada ya se encuentra procesada";
+                    }
+                    if (result.Confirmation == "0" && warranty.Codigo_Proceso != string.Empty)
+                    {
+                        result.Confirmation = "25";
+                        result.Msg_err = string.Format("La garantía ya fue ejecutada o liberada con el código del proceso #{0}", warranty.Codigo_Proceso);
+                    }
+                    if (result.Confirmation == "0" && warranty.Guarantee_sequencenumber != value.Guarantee_sequencenumber)
+                    {
+                        result.Confirmation = "22";
+                        result.Msg_err = "Sólo se pueden liberar/ejecutar garantías con la última secuencia";
+                    }
+                    if (result.Confirmation == "0" && warranty.Moneda != value.Moneda)
+                    {
+                        result.Confirmation = "24";
+                        result.Msg_err = "Tipo de moneda no corresponde a la original";
+                    }
 
-                    API.Core.Business.General.Mail.EnqueueSend("Default",
-                        Utilities.Helpers.Settings.IntegerValue("Default.CompanyId", 100),
-                        Utilities.Helpers.Settings.IntegerValue("Default.UserId", 0), 
-                        0, "Notify_ReleaseGuarantee", null, null, null, value, null);
+                    /// Si no se ha cambiado el codigo de confirmación por default, quiere decir que no hay error
+                    if (result.Confirmation == "0")
+                    {
+                        if (value.Metodo == "0")
+                            value.MetodoDesc = "ejecutar";
+                        if (value.Metodo == "1")
+                            value.MetodoDesc = "liberar";
+                        DataAccess.ElectronicWarranty.Update(warranty.ID, value.Metodo, value.codigo_proceso, value.Amount, value.Contents, value.cuenta_Cliente);
+
+                        API.Core.Business.General.Mail.EnqueueSend("Default",
+                            Utilities.Helpers.Settings.IntegerValue("Default.CompanyId", 100),
+                            Utilities.Helpers.Settings.IntegerValue("Default.UserId", 0),
+                            0, "Notify_ReleaseGuarantee", null, null, null, value, null);
+
+                        result.Confirmation = "0";
+                        result.Msg_err = "La solicitud fue procesada de forma exitosa";
+                    }
+                }
+                else
+                {
+                    result.Confirmation = "3";
+                    result.Msg_err = "La solicitud no puede estar vacia";
                 }
             }
-            else
+            catch (Exception ex)
             {
-                result.Confirmation = "3";
-                result.Msg_err = "La solicitud no puede estar vacia";
+                Architect.Utilities.Log.ErrorLog(ex);
+                result.Msg_err = "Error interno";
             }
-
-
+            Utilities.Log.TraceLog("Ejecutar_O_Liberar response", JsonConvert.SerializeObject(result), "ElectronicWarranty");
             return result;
         }
 
