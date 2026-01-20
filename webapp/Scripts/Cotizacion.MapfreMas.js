2985cd7556745a0affd0a36bd2c4690e84f7c266
roynera <royner.acosta@mapfrecr.com>
2026-01-20 17:01:32 -0600

    Se agrega al MapInputToObject el dato variable, para ser visualizado en el reporte de cotizacion

--- webapp/Scripts/Cotizacion.MapfreMas.js
+++ webapp/Scripts/Cotizacion.MapfreMas.js
@@ -248,6 +248,7 @@ app.CotizacionMapfreMas = (function () {
             cod_tip_vehiDesc: $("#cod_tip_vehi option:selected").text(),
             cod_uso_vehi: app.ui.GetDropDownNumericValue('#cod_uso_vehi'),
             cod_uso_vehiDesc: $("#cod_uso_vehi option:selected").text(),
+            COD_TIP_COM_VEHI_DESC: $("#COD_TIP_COM_VEHI option:selected").text(),
             MCA_CERO_KM: app.ui.GetRadioNumericValue('MCA_CERO_KM'),
             MCA_AUTO_GPS: app.ui.GetRadioNumericValue('MCA_AUTO_GPS'),
             MCA_AUTO_GPS_CMS: app.ui.GetRadioNumericValue('MCA_AUTO_GPS_CMS'),
